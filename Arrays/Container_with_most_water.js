951371403a65bb4220785349137aa4fb6d32422f
manpreet-codespace <manpreet.dev.85@gmail.com>
2026-06-26 00:15:35 +0530

    new: container_with_most_water

---
+++ Arrays/Container_with_most_water.js
@@ -0,0 +1,34 @@
+// let height= [1,8,6,2,5,4,8,3,7];
+
+function maxWater(height){
+    let left =0;
+    let right= height.length-1;
+
+    let maxArea =0;
+
+    while(left<right){
+        const width = right-left;
+        const currentHeight = Math.min(height[left],height[right]);
+        const area = width*currentHeight;
+
+        maxArea = Math.max(maxArea,area);
+
+        if(height[left]<height[right])
+        {
+            left++;
+
+        }
+        else{
+            right--;
+
+        }
+    }
+
+    return maxArea;
+
+
+}
+let height= [1,8,6,2,5,4,8,3,7];
+
+console.log(maxWater(height));
+
