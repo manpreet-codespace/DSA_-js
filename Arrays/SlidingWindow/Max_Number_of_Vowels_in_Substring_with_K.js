be781f68f13261a3a40e752e5fdf12116b00892a
manpreet-codespace <manpreet.dev.85@gmail.com>
2026-06-28 09:25:52 +0530

    new: sliding window patterns

--- Arrays/SlidingWindow/Max_Number_of_Vowels_in_Substring_with_K.js
+++ Arrays/SlidingWindow/Max_Number_of_Vowels_in_Substring_with_K.js
@@ -22,6 +22,41 @@ function maxVowels(nums,k)
 
 }
 
+// SLIDING WINDOW APPROACH
+
+function maxVowel(s,k){
+    let count =0;
+
+    for(let i=0;i<k;i++)
+    {
+        if(s[i] === "a" || s[i] === "e" || s[i] === "i" || s[i] === "o" || s[i] === "u")
+        {
+            count++;
+
+        }
+    }
+    let max = count;
+
+    for(let i =k;i<s.length;i++)
+    {
+        if(s[i-k] === "a" || s[i-k] === "e" || s[i-k] === "i" || s[i-k] === "o" || s[i-k] === "u"){
+            count--;
+
+        }
+
+        if(s[i] === "a" || s[i] === "e" || s[i] === "i" || s[i] === "o" || s[i] === "u"){
+            count++;
+
+        }
+        max = Math.max(max,count);
+    }
+
+    return max;
+    
+}
+
+
 let s="abciiidef";
 let k = 3;
 console.log(maxVowels(s,k));
+console.log(maxVowel(s,k));
