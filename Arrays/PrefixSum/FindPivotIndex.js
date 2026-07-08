d25df10331795b1aa69b3ccfee05f28cd60c8f40
manpreet-codespace <manpreet.dev.85@gmail.com>
2026-07-08 20:05:46 +0530

    new: Prefix sum question

---
+++ Arrays/PrefixSum/FindPivotIndex.js
@@ -0,0 +1,32 @@
+let arr = [1,2,3,4,5];  
+let total = 0;
+let leftSum = 0;
+let pivot = -1;
+
+for(let i = 0; i<arr.length;i++)
+{
+    total += arr[i];
+
+}
+
+for(let i = 0 ;i<arr.length;i++){
+    let rightSum = total -leftSum-arr[i];
+
+    if(leftSum === rightSum)
+    {
+        console.log(i);
+
+    }
+    else{
+        leftSum += arr[i];
+
+    }
+
+
+}
+
+console.log(pivot);
+
+
+
+
